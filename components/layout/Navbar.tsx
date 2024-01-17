81c1efd59065b10a705704ad5b8d3153681545e3
Piotr <piotr@bara.pl>
2024-01-17 16:32:15 +0100

    navbar-button-fix

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -11,11 +11,13 @@ export default function Navbar() {
   return (
     <Menu>
       <div className="w-full flex justify-center items-center h-28 bg-sky-300 dark:bg-sky-800">
-        <Menu.Button className="w-1/2">
-          <svg className='size-12 ml-8' aria-hidden="true" fill="none" strokeWidth={1.5} stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" strokeLinecap="round" strokeLinejoin="round" />
-          </svg>
-        </Menu.Button>
+        <div className="w-1/2">
+          <Menu.Button>
+            <svg className='size-12 ml-8' aria-hidden="true" fill="none" strokeWidth={1.5} stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" strokeLinecap="round" strokeLinejoin="round" />
+            </svg>
+          </Menu.Button>
+        </div>
         <a href='/' className="font-mono text-2xl w-1/2">ZS10.AI</a>
       </div>
       <Transition
